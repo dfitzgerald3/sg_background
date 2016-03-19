a64b3dbcb86681b67b46af6c4b49594f59919f76
dudz <braden.fitzgerald@gmail.com>
2016-03-19 13:13:42 -0400

    Updated the Hourly script to create index data for SPY, QQQ, and DIA

--- create_hourly_data.py
+++ create_hourly_data.py
@@ -6,6 +6,11 @@ import time
 import datetime
 
 
+###############################################################################
+#                       Create Stock SENTIMENT
+###############################################################################
+
+
 #Query MySQL and find values within that last hour
 time_greater_than = datetime.datetime.now() - datetime.timedelta(hours=1)
 unix_time_greater_than = time.mktime(time_greater_than.timetuple())
@@ -47,3 +52,51 @@ for i in range(len(stats)):
           (unix_now, stats.index[i], stats['mean'][i], stats['count'][i]))
 
 conn.commit()
+c.close()
+
+###############################################################################
+#                       Create INDEX SENTIMETN
+###############################################################################
+
+#Specify files with Weighting Schemes
+files = ['dia_weights.csv',
+         'qqq_weights.csv',
+         'spy_weights.csv']
+         
+#Establish Connection with DB
+c, conn = connection()
+
+
+#Read each file for each index, get the sentiment, weighting scheme, and volume
+for f in files:
+    path = '/braden/index_data/' + f
+    
+    df = pd.read_csv(path)    
+    df = df.drop(df.index[0])
+    
+    index = f[:3]
+    index_sent = 0
+    index_volume = 0
+    
+    for i in range(len(df)):
+        try:
+        
+            symbol = df.symbol.iloc[i]
+            weight = df.weight.iloc[i]
+            
+            c.execute("SELECT sentiment, time, volume FROM hourly WHERE time = (SELECT MAX(time) FROM hourly) AND symbol = %s", 
+                      (symbol, ))        
+                      
+            sent_data = c.fetchone()
+            
+            index_sent += sent_data[0] * weight            
+            time = sent_data[1]
+            index_volume += sent_data[2]
+            
+        except Exception:
+            continue
+
+    c.execute("INSERT INTO indexes (time, symbol, sentiment, volume) values (%s, %s, %s, %s)", 
+              (time, index, index_sent, index_volume))
+    
+    conn.commit()
